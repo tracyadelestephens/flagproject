ab288677112195f55a10bf202f8507859bad6a22
tracyadelestephens <tracyadele@gmail.com>
2015-12-21 20:21:39 -0600

    changes

--- colors.R
+++ colors.R
@@ -5,4 +5,13 @@ colordata <- read.csv("~/R/flag project/colordata.csv")
 flagcolors <- data.frame(colordata)
 colors <- flagcolors[ ,2:8]
 
+#import dataset
+alldata <- read.csv("~/R/flag project/flag.data_edited.csv")
+alldata <- data.frame(alldata)
+
+#regression with color data
+#area & population
+col_area <- cbind(colors, alldata$area)
+area_lm_fit <- lm(alldata$area ~ ., data = col_area) 
+area_lm_preds <- predict(area_lm_fit, col_area)
 
--- partitionforanalysis.R
+++ partitionforanalysis.R
@@ -7,13 +7,14 @@ fvars <- c("nbars", "nstripes", "ncolors", "red", "green", "blue", "gold", "whit
 names(flagdf)[1] <- "name"
 names(flagdf)[2:7] <- cvars
 names(flagdf)[8:30] <- fvars
+write.csv(flagdf, "flag.data_edited.csv", row.names = FALSE)
 
 #just color data
 flagcols <- flagdf[ ,fvars[3:11]]
 mainhue.inwords <- flagdf$mainhue
 flagcols$mainhue <- match(flagcols$mainhue, rev(names(sort(table(flagcols$mainhue)))))
 flagcols[, 2:9] <- apply(flagcols[, 2:9], 2, as.factor)
-write.csv(flagcols, "colordata.csv", row.names = TRUE, col.names = TRUE)
+write.csv(flagcols, "colordata.csv", row.names = FALSE)
 
 #just country characteristics
 cc <- flagdf[ ,cvars]
