727c989828e5f466fee0fe298198aab8fe5f5c2b
tracyadelestephens <tracyadele@gmail.com>
2015-12-21 19:59:23 -0600

    add color export

--- partitionforanalysis.R
+++ partitionforanalysis.R
@@ -1,3 +1,4 @@
+flag.data <- read.csv("~/R/flag project/flag.data.csv", header=FALSE)
 flagdf <- data.frame(flag.data)
 cvars <- c("cont", "hemi", "area", "pop", "lang", "rel")
 fvars <- c("nbars", "nstripes", "ncolors", "red", "green", "blue", "gold", "white", "black", 
@@ -7,11 +8,12 @@ names(flagdf)[1] <- "name"
 names(flagdf)[2:7] <- cvars
 names(flagdf)[8:30] <- fvars
 
-#just colors
+#just color data
 flagcols <- flagdf[ ,fvars[3:11]]
 mainhue.inwords <- flagdf$mainhue
 flagcols$mainhue <- match(flagcols$mainhue, rev(names(sort(table(flagcols$mainhue)))))
 flagcols[, 2:9] <- apply(flagcols[, 2:9], 2, as.factor)
+write.csv(flagcols, "colordata.csv", row.names = TRUE, col.names = TRUE)
 
 #just country characteristics
 cc <- flagdf[ ,cvars]
