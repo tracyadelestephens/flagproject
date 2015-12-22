d55475462150e4bd7c38b7419a4d7ac8572bc4ed
tracyadelestephens <tracyadele@gmail.com>
2015-12-21 20:30:34 -0600

    color

--- colors.R
+++ colors.R
@@ -10,8 +10,15 @@ alldata <- read.csv("~/R/flag project/flag.data_edited.csv")
 alldata <- data.frame(alldata)
 
 #regression with color data
-#area & population
+#area
 col_area <- cbind(colors, alldata$area)
-area_lm_fit <- lm(alldata$area ~ ., data = col_area) 
+area_lm_fit <- lm(col_area[ ,8] ~ red + green + blue + gold + white + black + orange, 
+                  data = col_area) 
 area_lm_preds <- predict(area_lm_fit, col_area)
+#population
+col_pop <- cbind(colors, alldata$pop)
+pop_lm_fit <- lm(col_pop[ ,8] ~ red + green + blue + gold + white + black + orange, 
+                 data = col_pop) 
+pop_lm_preds <- predict(area_lm_fit, col_pop)
+#need to group colors
 
